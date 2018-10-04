9dfd76da342780e9b109b2fe9abacc5cee2885a8
rohomedesrius <rogeret9669@hotmail.es>
2018-10-04 15:52:44 +0200

    Camera Unity-like almost done
    - Right Click Rotates +
    - Right Click + WASD moves camera
    - ALT + Left Click Rotates -

--- LastHope Engine/ModuleCamera3D.cpp
+++ LastHope Engine/ModuleCamera3D.cpp
@@ -11,8 +11,8 @@ ModuleCamera3D::ModuleCamera3D(Application* app, bool start_enabled) : Module(ap
 	Y = vec3(0.0f, 1.0f, 0.0f);
 	Z = vec3(0.0f, 0.0f, 1.0f);
 
-	Position = vec3(0.0f, 0.0f, 5.0f);
-	Reference = vec3(0.0f, 0.0f, 0.0f);
+Position = vec3(0.0f, 0.0f, 5.0f);
+Reference = vec3(0.0f, 0.0f, 0.0f);
 }
 
 ModuleCamera3D::~ModuleCamera3D()
@@ -58,52 +58,114 @@ update_status ModuleCamera3D::Update(float dt)
 
 	vec3 Movement;
 
-	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) Movement += Forward;
-	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) Movement -= Forward;
-	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) Movement -= Right;
-	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) Movement += Right;
-	if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) Movement += Up;
-	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) Movement -= Up;
-
-	Position += Movement;
-	Reference += Movement;
-
 	// Mouse motion ----------------
 
-	if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+	//ALT rotation-----------------------
+
+	if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT)
 	{
-
-		int dx = -App->input->GetMouseXMotion();
-		int dy = -App->input->GetMouseYMotion();
-
-		float Sensitivity = 0.25f;
-
-		Position -= Reference;
-
-		if (dx != 0)
+		if(App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT)
 		{
-			float DeltaX = (float)dx * Sensitivity;
+			int dx = App->input->GetMouseXMotion();
+			int dy = App->input->GetMouseYMotion();
 
-			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-		}
+			float Sensitivity = 0.25f;
 
-		if (dy != 0)
-		{
-			float DeltaY = (float)dy * Sensitivity;
+			Position -= Reference;
 
-			Y = rotate(Y, DeltaY, X);
-			Z = rotate(Z, DeltaY, X);
-
-			if (Y.y < 0.0f)
+			if (dx != 0)
 			{
-				Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
-				Y = cross(Z, X);
+				float DeltaX = (float)dx * Sensitivity;
+
+				X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+				Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+				Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
 			}
+
+			if (dy != 0)
+			{
+				float DeltaY = (float)dy * Sensitivity;
+
+				Y = rotate(Y, DeltaY, X);
+				Z = rotate(Z, DeltaY, X);
+
+				if (Y.y < 0.0f)
+				{
+					Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
+					Y = cross(Z, X);
+				}
+			}
+
+			Position = Reference + Z * length(Position);
 		}
 
-		Position = Reference + Z * length(Position);
+		else if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+		{
+			int dx = -App->input->GetMouseXMotion();
+			int dy = -App->input->GetMouseYMotion();
+
+			float Sensitivity = 0.25f;
+
+			Position -= Reference;
+
+			if (dx != 0)
+			{
+				//TODO
+			}
+
+			if (dy != 0)
+			{
+				//TODO
+			}
+
+			Position = Reference + Z * length(Position);
+		}
+	}
+	else
+	{
+		if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+		{
+			//Unity-Like WSAD
+			if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) Movement += Forward;
+			if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) Movement -= Forward;
+			if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) Movement -= Right;
+			if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) Movement += Right;
+			if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) Movement += Up;
+			if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) Movement -= Up;
+			//------------------------------------------------------------------------
+
+			int dx = -App->input->GetMouseXMotion();
+			int dy = -App->input->GetMouseYMotion();
+
+			float Sensitivity = 0.25f;
+
+			Position -= Reference;
+
+			if (dx != 0)
+			{
+				float DeltaX = (float)dx * Sensitivity;
+
+				X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+				Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+				Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			}
+
+			if (dy != 0)
+			{
+				float DeltaY = (float)dy * Sensitivity;
+
+				Y = rotate(Y, DeltaY, X);
+				Z = rotate(Z, DeltaY, X);
+
+				if (Y.y < 0.0f)
+				{
+					Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
+					Y = cross(Z, X);
+				}
+			}
+
+			Position = Reference + Z * length(Position);
+		}
 	}
 
 	// Mouse wheel -----------------------
@@ -123,6 +185,9 @@ update_status ModuleCamera3D::Update(float dt)
 	}
 
 	Position += Reference;
+
+	Position += Movement;
+	Reference += Movement;
 
 	// Recalculate matrix -------------
 	CalculateViewMatrix();
