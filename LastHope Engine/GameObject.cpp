f361606453fa7c94703d429a7803560f9b2afc8e
GottaCodeHarder <masterkonnen@gmail.com>
2018-11-03 16:14:17 +0100

    Working on GameObjects

---
+++ LastHope Engine/GameObject.cpp
@@ -0,0 +1,70 @@
+#include "GameObject.h"
+#include "Application.h"
+
+
+GameObject::GameObject()
+{
+}
+
+GameObject::GameObject(std::string name, bool is_active, bool is_static, GameObject * parent)
+{
+	this->name = name;
+	this->is_active = is_active;
+	this->is_static = is_static;
+	this->parent = parent;
+}
+
+GameObject::~GameObject()
+{
+}
+
+void GameObject::AddChild(GameObject * child)
+{
+	if (child != nullptr)
+	{
+		children.push_back(child);
+	}
+}
+
+void GameObject::RemoveChild(GameObject * child)
+{
+	if (child != nullptr)
+	{
+		std::vector<GameObject*>::iterator item = children.begin();
+
+		while (item != children.end())
+		{
+			if (*item == child)
+			{
+				// modify child's parent
+				children.erase(item);
+				break;
+			}
+			++item;
+		}
+	}
+}
+
+void GameObject::RemoveChildren()
+{
+	children.clear();
+}
+
+void GameObject::AddComponent(Component * component)
+{
+}
+
+void GameObject::Enable()
+{
+	is_active = false;
+}
+
+void GameObject::Disable()
+{
+	is_active = false;
+}
+
+void GameObject::SetStatic(bool set_static)
+{
+	is_static = set_static;
+}
